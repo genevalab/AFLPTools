ee23773d54dca35e89bb729790c869295ae2f907
geneva <geneva@crescatscientia.com>
2014-09-04 17:39:23 -0400

    Added and updated core functions

--- R/AFLP_filter.R
+++ R/AFLP_filter.R
@@ -8,16 +8,9 @@
 #' @examples
 #' AFLP_filter(myStructureFile,filteredStructureFile)
 
-
 AFLP_filter <- function(infile,outfile){
-  dat <- read.table(skip=1, infile)
-  row.names(dat) <- dat[,1]
-  dat <- dat[,-1]
+  dat <- read.structure(infile)
   filtered_dat <- dat[,which(colSums(dat)!=0)]
-  colnames(filtered_dat)  <- rep(0,ncol(filtered_dat))
-  write.table(filtered_dat,outfile, quote=F, sep="\t")
   cat("Removed",(ncol(dat)-ncol(filtered_dat)),"loci\n", sep=" ")
-  cat("Final dataset dimensions:\n")
-  cat(nrow(filtered_dat),"individuals\n")
-  cat((ncol(filtered_dat)-1),"loci\n")
+  write.structure(filtered_dat, outfile)
 }
